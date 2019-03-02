df2cdf16faa39e064e1db0d64f28b35befb2da68
Stephen Robinson <stephen@stephen-robinson.co.uk>
2019-03-02 18:06:48 +0000

    Initial Alexa SMAPI Client gem

---
+++ lib/alexa/smapi.rb
@@ -0,0 +1,8 @@
+require "alexa/smapi/version"
+
+module Alexa
+  module SMAPI
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/alexa/smapi/version.rb
@@ -0,0 +1,5 @@
+module Alexa
+  module SMAPI
+    VERSION = "0.1.0"
+  end
+end
