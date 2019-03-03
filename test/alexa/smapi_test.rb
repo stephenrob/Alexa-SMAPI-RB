c697a0fc6bb387e017144e1e07c503d1106a19c3
Stephen Robinson <stephen@stephen-robinson.co.uk>
2019-03-03 15:33:36 +0000

    Remove unnecessary test

--- test/alexa/smapi_test.rb
+++ test/alexa/smapi_test.rb
@@ -4,8 +4,4 @@ class Alexa::SMAPITest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Alexa::SMAPI::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
