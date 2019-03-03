a243f0d1bc673f19976cf5677079b42b8dac642b
Stephen Robinson <stephen@stephen-robinson.co.uk>
2019-03-03 16:05:03 +0000

    Add minitest ci config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,11 @@ require "minitest/autorun"
 require 'mocha/minitest'
 require 'webmock/minitest'
 require 'vcr'
+require 'minitest/ci'
+
+if ENV["CIRCLECI"]
+  Minitest::Ci.report_dir = "#{ENV["CIRCLE_TEST_REPORTS"]}/reports"
+end
 
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
