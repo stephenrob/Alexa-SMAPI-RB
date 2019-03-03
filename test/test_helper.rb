9d2c6035cf2595b4e3b64c5361b1222642b08f95
Stephen Robinson <stephen@stephen-robinson.co.uk>
2019-03-03 15:32:16 +0000

    Add required gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'simplecov'
+SimpleCov.start
+
 require "alexa/smapi"
 
 require "minitest/autorun"
+require 'mocha/minitest'
+require 'webmock/minitest'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
